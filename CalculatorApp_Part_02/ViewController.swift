74803bd8247f675a0c7fde5f0b2ddca64bc7fdef
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-10-05 09:36:08 -0400

    Added Addition, subtraction, multiplication, division and equal to operations

--- CalculatorApp_Part_02/ViewController.swift
+++ CalculatorApp_Part_02/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController
     {
+        var operand1: Double = 0.0
+        var operand2: Double = 0.0
+        var result: Double = 0.0
+        var activeOperation: String = ""
+        var activeNumber: String = ""
+        var operation_result = 0.0;
+        var old_operator = "";
+    var is_active = true;
 
     @IBOutlet weak var ResultLabel: UILabel!
     
@@ -19,12 +27,112 @@ class ViewController: UIViewController
 
     @IBAction func OnNumberButton_Press(_ sender: UIButton)
     {
-        print(sender.titleLabel?.text)
+        switch sender.titleLabel?.text! {
+        case "C":
+            ResultLabel.text! = "0"
+            operand1=0.0
+            operand2=0.0
+            operation_result = 0.0
+            activeOperation = ""
+            old_operator = ""
+            is_active = true;
+        case "⌫":
+            ResultLabel.text!.removeLast()
+            if((ResultLabel.text!.count < 1) || (ResultLabel.text! == "-"))
+            {
+                ResultLabel.text! = "0"
+            }
+        case ".":
+            if(!ResultLabel.text!.contains("."))
+            {
+                ResultLabel.text! += "."
+            }
+        case "+/-":
+            if(ResultLabel.text! != "0")
+            {
+                if(!ResultLabel.text!.contains("-"))
+                {
+                    ResultLabel.text!.insert("-", at: ResultLabel.text!.startIndex)
+                }
+                else
+                {
+                    ResultLabel.text!.remove(at: ResultLabel.text!.startIndex)
+                }
+            }
+        default:
+            if(is_active || ResultLabel.text! == "0")
+            {
+                ResultLabel.text = sender.titleLabel!.text!
+                is_active = false;
+            } else
+            {
+                if(ResultLabel.text!.count > 7 ) {
+                    return
+                }
+                ResultLabel.text! += sender.titleLabel!.text!
+            }
+        }
+        
+        if(ResultLabel.text!.contains("."))
+        {
+           print(Double(ResultLabel.text!)!)
+        }
+        else
+        {
+           print(Int(ResultLabel.text!)!)
+        }
+        
     }
     
     @IBAction func OnOperatorButton_Press(_ sender: UIButton)
     {
-        print(sender.titleLabel?.text)
+        activeOperation = sender.titleLabel!.text!
+        
+        if(old_operator == "") {
+            old_operator = activeOperation
+            is_active = true;
+        }
+        
+        if(operation_result != 0.0) {
+            return
+        }
+        if(operand1 != 0.0) {
+            operand2 = Double(ResultLabel.text!)!
+        } else {
+            operand1 = Double(ResultLabel.text!)!
+        }
+        
+        if(operand2 == 0.0 && activeOperation != "=")
+        {
+            //print("operand2")
+            return
+        }
+        
+        switch old_operator
+        {
+        case "+":
+            print(operand2)
+            operation_result = operand1 + operand2
+        case "-":
+            operation_result = operand1 - operand2
+        case "X":
+            operation_result = operand1 * operand2
+        case "÷":
+            operation_result = operand1 / operand2
+        case "%":
+            print(sender.titleLabel!.text!)
+            print(old_operator)
+        case "=":
+            operation_result = operand1;
+        default:
+            print("Error....")
+        }
+        operand1 = operation_result
+        operand2 = 0.00;
+        old_operator = activeOperation
+        is_active = true
+        ResultLabel.text! = String(round(pow(10, 9)*operation_result)/pow(10, 9))
+        operation_result = 0.00;
     }
 }
 
